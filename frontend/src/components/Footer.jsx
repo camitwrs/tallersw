7c11425412403e984808b36f7bcacf3785f20222
Jan <jan.houter.h@mail.pucv.cl>
2024-10-07 14:25:37 -0300

    Update HomePage Responsiva

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Footer(){
-    return (
-        <footer className="bg-p-l-Gray py-8 text-center">
-            <p className="text-black text-sm">
-                © 2024 SAUKKOTECH TODOS LOS DERECHOS RESERVADOS.
-            </p>
-        </footer>
-    );
+function Footer() {
+  return (
+    <footer className="bg-p-l-Gray py-4 text-center">
+      <p className="text-black text-sm">
+        © 2024 SAUKKOTECH TODOS LOS DERECHOS RESERVADOS.
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,33 +1,39 @@
 import React from "react";
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import nutria_nobg from "../assets/nutria_nobg.png";
 
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      
+
       <main className="flex-grow bg-white flex justify-center items-center">
         <div className="container mx-auto px-4 flex flex-col lg:flex-row items-center justify-between lg:px-12">
-          
-          <div className="lg:pl-12">
-            <h1 className="text-5xl font-bold text-black mb-4">
+          <div className="lg:pl-12 text-center lg:text-left mt-8 sm:mt-12 lg:mt-16">
+            <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-black mb-4">
               Los Educadores de Emprendimiento <br />
               son Mamíferos Marinos.
-           </h1>
-            <p className="text-3xl text-p-Gray mb-6">
+            </h1>
+            <p className="text-lg sm:text-2xl lg:text-3xl text-p-Gray mb-6">
               Descubre con nosotros tu perfil educativo.
             </p>
-            <Link to="/cuestionario" className="bg-p-Blue text-white px-6 py-5 rounded-full text-lg inline-block hover:bg-blue-800 transition-colors">
-            &gt;&gt; Haz el cuestionario aquí
+            <Link
+              to="/cuestionario"
+              className="bg-p-Blue text-white px-6 py-3 sm:px-8 sm:py-4 lg:px-10 lg:py-5 rounded-full text-base sm:text-lg lg:text-xl inline-block hover:bg-blue-800 transition-colors"
+            >
+              &gt;&gt; Haz el cuestionario aquí
             </Link>
           </div>
 
-          <div>
-            <img src={nutria_nobg} alt="Mamífero Marino" className="w-auto h-full" />
+          <div className="mt-8 lg:mt-0">
+            <img
+              src={nutria_nobg}
+              alt="Mamífero Marino"
+              className="w-64 sm:w-80 lg:w-96 xl:w-[36rem] h-auto"
+            />
           </div>
         </div>
       </main>
